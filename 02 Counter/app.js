4ad29063412ce2c5ed89348e0b299b997e52318c
gilmore-1 <basti.xachapuridze@gmail.com>
2022-10-02 16:40:13 +0400

    add value style

--- 02 Counter/app.js
+++ 02 Counter/app.js
@@ -8,7 +8,6 @@ btns.forEach(btn => {
     btn.addEventListener("click", (e) => {
         const styles = e.currentTarget.classList;
         if(styles.contains('decrease')){
-            if(count <= 0) return;
             count --;
             
         }
@@ -17,6 +16,16 @@ btns.forEach(btn => {
         }
         else{
             count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "brown";
+        }
+        if(count === 0){
+            value.style.color = "black";
+
         }
         value.textContent = count;
     })
